af27fca29f431d896e1338110a92e2963be4d135
Bernard Duggan <bernardd@users.noreply.github.com>
2018-11-01 08:14:55 +1100

    Expose the ability to generate cursors from records (#32)

--- test/paginator_test.exs
+++ test/paginator_test.exs
@@ -648,6 +648,16 @@ defmodule PaginatorTest do
     end
   end
 
+  test "per-record cursor generation", %{
+    payments: {p1, _p2, _p3, _p4, _p5, _p6, p7, _p8, _p9, _p10, _p11, _p12}
+  } do
+    assert Paginator.cursor_for_record(p1, [:charged_at, :id])
+    == encode_cursor([p1.charged_at, p1.id])
+
+    assert Paginator.cursor_for_record(p7, [:amount])
+    == encode_cursor([p7.amount])
+  end
+
   defp to_ids(entries), do: Enum.map(entries, & &1.id)
 
   defp create_customers_and_payments(_context) do
