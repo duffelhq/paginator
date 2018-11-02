38ac97ecff789d656c23f3eb7e1c6bf216cce467
Nick Vernij <hello@nickforall.nl>
2018-11-02 16:10:21 +0100

    First version of advanced sort directions

--- lib/paginator/ecto/query.ex
+++ lib/paginator/ecto/query.ex
@@ -17,7 +17,17 @@ defmodule Paginator.Ecto.Query do
     paginate(queryable, Config.new(opts))
   end
 
-  defp filter_values(query, cursor_fields, values, operator) do
+  defp filter_values(query, cursor_fields, values, :lt) do
+    operator_list = Enum.map(cursor_fields, fn x -> {x, :lt} end)
+    filter_values(query, cursor_fields, values, operator_list)
+  end
+
+  defp filter_values(query, cursor_fields, values, :gt) do
+    operator_list = Enum.map(cursor_fields, fn x -> {x, :gt} end)
+    filter_values(query, cursor_fields, values, operator_list)
+  end
+
+  defp filter_values(query, cursor_fields, values, operators) when is_list(operators) do
     sorts =
       cursor_fields
       |> Enum.zip(values)
@@ -30,7 +40,7 @@ defmodule Paginator.Ecto.Query do
         dynamic = true
 
         dynamic =
-          case operator do
+          case Keyword.get(operators, column) do
             :lt ->
               dynamic([q], field(q, ^column) < ^value and ^dynamic)
 
@@ -133,6 +143,112 @@ defmodule Paginator.Ecto.Query do
     query
     |> filter_values(cursor_fields, after_values, :lt)
     |> filter_values(cursor_fields, before_values, :gt)
+  end
+
+  defp maybe_where(query, %Config{
+         after: nil,
+         before: nil,
+         cursor_fields: cursor_fields,
+         sort_direction: sort_direction
+       })
+       when is_list(sort_direction) do
+    validate_sort_direction_list!(sort_direction, cursor_fields)
+
+    query
+  end
+
+  defp maybe_where(query, %Config{
+         after_values: after_values,
+         before: nil,
+         cursor_fields: cursor_fields,
+         sort_direction: sort_direction
+       })
+       when is_list(sort_direction) do
+    validate_sort_direction_list!(sort_direction, cursor_fields)
+
+    query
+    |> filter_values(
+      cursor_fields,
+      after_values,
+      convert_sort_direction_list(sort_direction, :before)
+    )
+  end
+
+  defp maybe_where(query, %Config{
+         after: nil,
+         before_values: before_values,
+         cursor_fields: cursor_fields,
+         sort_direction: sort_direction
+       })
+       when is_list(sort_direction) do
+    validate_sort_direction_list!(sort_direction, cursor_fields)
+
+    query
+    |> filter_values(
+      cursor_fields,
+      before_values,
+      convert_sort_direction_list(sort_direction, :before)
+    )
+    |> reverse_order_bys()
+  end
+
+  defp maybe_where(query, %Config{
+         after_values: after_values,
+         before_values: before_values,
+         cursor_fields: cursor_fields,
+         sort_direction: sort_direction
+       })
+       when is_list(sort_direction) do
+    validate_sort_direction_list!(sort_direction, cursor_fields)
+
+    query
+    |> filter_values(
+      cursor_fields,
+      after_values,
+      convert_sort_direction_list(sort_direction, :after)
+    )
+    |> filter_values(
+      cursor_fields,
+      before_values,
+      convert_sort_direction_list(sort_direction, :before)
+    )
+  end
+
+  # performs checks on the provided config before we run queries
+  defp validate_sort_direction_list!(direction, cursor_fields) do
+    # the list must be a keyword list
+    unless Keyword.keyword?(direction),
+      do: raise("Expected sort direction to either be a keyword list, :asc or :desc")
+
+    # Check whether all cursor fields have a sorting direction associated
+    missing_fields = Enum.filter(cursor_fields, fn x -> !Keyword.has_key?(direction, x) end)
+
+    # if there are fields missing a direction, raise an error informing about the missing fields
+    if length(missing_fields) > 0 do
+      missing_fields = Enum.join(missing_fields, ", ")
+      raise("There is no sorting direction provided for the fields #{missing_fields}")
+    end
+
+    # lastly, check whether the values are either ascending or descending
+    Enum.each(direction, fn {key, value} ->
+      if value != :desc or value != :asc,
+        do: raise("Value for #{key} is invalid, please use either :desc or :asc")
+    end)
+  end
+
+  # converts a column direction to a conditional, for example {column: :desc} to {column: :lt}
+  defp convert_sort_direction_list(direction_list, cursor_type) do
+    Enum.map(direction_list, fn {key, direction} ->
+      operator =
+        case {cursor_type, direction} do
+          {:after, :asc} -> :lt
+          {:after, :desc} -> :gt
+          {:before, :asc} -> :gt
+          {:before, :desc} -> :lt
+        end
+
+      {key, operator}
+    end)
   end
 
   #  In order to return the correct pagination cursors, we need to fetch one more
