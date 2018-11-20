cc022be58ba4b35e934cce304d5ffc1cc77e7559
Ivan Mironov <ivan.mironov@gmail.com>
2018-11-20 05:20:21 -0500

    Update to Ecto 3 (#40)
    Will add backwards compatibility to Ecto 2 in another commit.

--- config/test.exs
+++ config/test.exs
@@ -3,7 +3,6 @@ use Mix.Config
 config :paginator, ecto_repos: [Paginator.Repo]
 
 config :paginator, Paginator.Repo,
-  adapter: Ecto.Adapters.Postgres,
   pool: Ecto.Adapters.SQL.Sandbox,
   username: "postgres",
   password: "postgres",
