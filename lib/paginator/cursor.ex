65b9d022fc884e51b37e6c43373f71c1647f0975
Sviatoslav Bulbakha <mail@ssbb.me>
2021-09-28 07:02:02 +0300

    Merge

--- lib/paginator/cursor.ex
+++ lib/paginator/cursor.ex
@@ -1,17 +1,38 @@
 defmodule Paginator.Cursor do
   @moduledoc false
-
   def decode(nil), do: nil
 
   def decode(encoded_cursor) do
     encoded_cursor
     |> Base.url_decode64!()
-    |> Plug.Crypto.non_executable_binary_to_term([:safe])
+    |> :erlang.binary_to_term([:safe])
+    |> Enum.map(&Paginator.Cursor.Decode.convert/1)
   end
 
   def encode(values) when is_map(values) do
     values
+    |> Enum.map(&Paginator.Cursor.Encode.convert/1)
     |> :erlang.term_to_binary()
     |> Base.url_encode64()
   end
+end
+
+defprotocol Paginator.Cursor.Encode do
+  @fallback_to_any true
+
+  def convert(term)
+end
+
+defprotocol Paginator.Cursor.Decode do
+  @fallback_to_any true
+
+  def convert(term)
+end
+
+defimpl Paginator.Cursor.Encode, for: Any do
+  def convert(term), do: term
+end
+
+defimpl Paginator.Cursor.Decode, for: Any do
+  def convert(term), do: term
 end
