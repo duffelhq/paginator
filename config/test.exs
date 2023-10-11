2e20946a3be0941e5203a0935d7d373ecd823caa
Blake Kostner <git@btkostner.io>
2023-10-11 11:06:50 -0600

    setup postgres for tests

--- config/test.exs
+++ config/test.exs
@@ -3,9 +3,11 @@ import Config
 config :paginator, ecto_repos: [Paginator.Repo]
 
 config :paginator, Paginator.Repo,
-  pool: Ecto.Adapters.SQL.Sandbox,
+  port: 5432,
   username: "postgres",
   password: "postgres",
-  database: "paginator_test"
+  database: "paginator_test#{System.get_env("MIX_TEST_PARTITION")}",
+  hostname: "localhost",
+  pool: Ecto.Adapters.SQL.Sandbox
 
 config :logger, :console, level: :warning
